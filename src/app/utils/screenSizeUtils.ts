ab8eb08a2f87de12f43f39dc6435f74c28b2e276
wakenedo <a_nuner@outlook.com>
2024-10-10 16:21:52 -0300

    Fixing prettier and eslint build errors

--- src/app/utils/screenSizeUtils.ts
+++ src/app/utils/screenSizeUtils.ts
@@ -1,7 +1,7 @@
-import { useScreenSize } from "../hooks/useScreenSize";
+import { useEffect, useState } from 'react';
 
 // Define TailwindCSS breakpoints with explicit types
-export type BreakpointKeys = "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+export type BreakpointKeys = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 
 const breakpoints: Record<BreakpointKeys, number> = {
   xs: 0,
@@ -9,16 +9,51 @@ const breakpoints: Record<BreakpointKeys, number> = {
   md: 768,
   lg: 1024,
   xl: 1280,
-  "2xl": 1536,
+  '2xl': 1536,
+};
+
+export const _screenSize = () => {
+  const [screenSize, setScreenSize] = useState<BreakpointKeys | null>(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+
+      if (width < breakpoints.sm) {
+        setScreenSize('xs');
+      } else if (width < breakpoints.md) {
+        setScreenSize('sm');
+      } else if (width < breakpoints.lg) {
+        setScreenSize('md');
+      } else if (width < breakpoints.xl) {
+        setScreenSize('lg');
+      } else if (width < breakpoints['2xl']) {
+        setScreenSize('xl');
+      } else {
+        setScreenSize('2xl');
+      }
+    };
+
+    // Set initial size
+    handleResize();
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return screenSize;
 };
 
 // Utility function to apply values based on breakpoints
 export const applyBreakpointValues = (
   breakpointValues: Partial<Record<BreakpointKeys, string | number>>,
-  fallback: string = "0"
+  fallback: string = '0'
 ) => {
-  const screenSize = useScreenSize();
-  console.log("Current screen size in applyBreakpointValues:", screenSize);
+  const screenSize = _screenSize();
+  console.log('Current screen size in applyBreakpointValues:', screenSize);
 
   if (!screenSize) return fallback; // Return fallback if screen size is not available
 
@@ -27,7 +62,7 @@ export const applyBreakpointValues = (
   sortedBreakpoints.sort((a, b) => breakpoints[a] - breakpoints[b]);
 
   // Log the sorted breakpoints for debugging
-  console.log("Sorted breakpoints:", sortedBreakpoints);
+  console.log('Sorted breakpoints:', sortedBreakpoints);
 
   // Variable to hold the returned value
   let returnValue: string = fallback;
@@ -48,7 +83,7 @@ export const applyBreakpointValues = (
     }
   }
 
-  console.log("Final return value:", returnValue);
+  console.log('Final return value:', returnValue);
 
   return returnValue; // Return the last matching value
 };
--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,25 +1,32 @@
-import { FC, ButtonHTMLAttributes } from "react";
-import { twMerge } from "tailwind-merge";
+import { FC, ButtonHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: string;
-  variant: "primaryFill" | "secondaryFill" | "tertiaryFill" | "primaryOutline" | "secondaryOutline" | "tertiaryOutline" | "default";
+  variant:
+    | 'primaryFill'
+    | 'secondaryFill'
+    | 'tertiaryFill'
+    | 'primaryOutline'
+    | 'secondaryOutline'
+    | 'tertiaryOutline'
+    | 'default';
   className?: string;
   disabled?: boolean;
 }
 
 const ButtonColor = {
-  primaryFill: "bg-primary-dark hover:bg-primary-light focus:ring-primary-dark",
+  primaryFill: 'bg-primary-dark hover:bg-primary-light focus:ring-primary-dark',
   primaryOutline:
-    "text-primary-dark bg-transparent border border-primary-dark hover:bg-primary/10 focus:ring-primary-dark",
+    'text-primary-dark bg-transparent border border-primary-dark hover:bg-primary/10 focus:ring-primary-dark',
   secondaryFill:
-    "bg-secondary-dark hover:bg-secondary-light focus:ring-secondary-dark",
+    'bg-secondary-dark hover:bg-secondary-light focus:ring-secondary-dark',
   secondaryOutline:
-    "text-secondary-dark bg-transparent border border-secondary-dark hover:bg-secondary-dark focus:ring-secondary-dark",
+    'text-secondary-dark bg-transparent border border-secondary-dark hover:bg-secondary-dark focus:ring-secondary-dark',
   tertiaryFill:
-    "bg-tertiary-dark hover:bg-tertiary-light focus:ring-tertiary-dark",
+    'bg-tertiary-dark hover:bg-tertiary-light focus:ring-tertiary-dark',
   tertiaryOutline:
-    "text-tertiary-dark bg-transparent border border-tertiary-dark hover:bg-tertiary-dark focus:ring-tertiary-dark",
+    'text-tertiary-dark bg-transparent border border-tertiary-dark hover:bg-tertiary-dark focus:ring-tertiary-dark',
 };
 
 const Button: FC<ButtonProps> = ({
@@ -33,24 +40,24 @@ const Button: FC<ButtonProps> = ({
     <button
       className={twMerge(
         `w-fit px-4 py-2 rounded-md text-white font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150 ${className}`,
-        
-        disabled && "opacity-50 cursor-not-allowed",
+
+        disabled && 'opacity-50 cursor-not-allowed',
         (() => {
           switch (variant) {
-            case "primaryFill":
+            case 'primaryFill':
               return ButtonColor.primaryFill;
-            case "primaryOutline":
+            case 'primaryOutline':
               return ButtonColor.primaryOutline;
-            case "secondaryFill":
+            case 'secondaryFill':
               return ButtonColor.secondaryFill;
-            case "secondaryOutline":
+            case 'secondaryOutline':
               return ButtonColor.secondaryOutline;
-            case "tertiaryFill":
+            case 'tertiaryFill':
               return ButtonColor.tertiaryFill;
-            case "tertiaryOutline":
+            case 'tertiaryOutline':
               return ButtonColor.tertiaryOutline;
             default:
-              return "";
+              return '';
           }
         })()
       )}
