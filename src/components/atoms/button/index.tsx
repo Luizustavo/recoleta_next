1f5d60b4c9bbde674437316dc420995bc268272c
Luizustavo <luizg.ss@outlook.com>
2024-10-10 00:38:28 -0300

    ✨ feat:  Create Home page with header, video, and additional info; Add Header component; Adjust Button component

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: string;
-  variant: "primaryFill" | "secondaryFill" | "tertiaryFill" | "primaryOutline" | "secondaryOutline" | "tertiaryOutline";
+  variant: "primaryFill" | "secondaryFill" | "tertiaryFill" | "primaryOutline" | "secondaryOutline" | "tertiaryOutline" | "default";
   className?: string;
   disabled?: boolean;
 }
@@ -32,8 +32,8 @@ const Button: FC<ButtonProps> = ({
   return (
     <button
       className={twMerge(
-        "w-fit px-4 py-2 rounded-md text-white font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150",
-        className,
+        `w-fit px-4 py-2 rounded-md text-white font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150 ${className}`,
+        
         disabled && "opacity-50 cursor-not-allowed",
         (() => {
           switch (variant) {
