4ad019a9a08fbb1d30214f314443b17ed5f632bc
Luizustavo <luizg.ss@outlook.com>
2024-10-28 10:06:53 -0300

    refactor: Update layout and styling in about page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function About() {
   return (
-    <main className="grid grid-cols-2 ">
+    <main className="grid grid-cols-1 md:grid-cols-2 h-screen">
       <video
         src="/videos/residuo_about.mp4"
         autoPlay
@@ -13,15 +13,15 @@ export default function About() {
         className="max-h-screen w-full object-cover"
       ></video>
 
-      <div className="flex flex-col items-start px-56 py-24 gap-12 max-h-screen">
-        <h1 className="text-6xl font-bold flex flex-col leading-tight first-letter:font-light">
+      <section className="flex flex-col items-start px-10 md:px-24 lg:px-40 py-10 md:py-20 gap-8 overflow-y-auto max-h-screen">
+        <h1 className="text-5xl font-bold flex flex-col leading-tight first-letter:font-light">
           Conheça o{' '}
           <span className="flex">
             <p className="text-primary-orange">Re</p>
             <p className="text-primary-green">Coleta</p>
           </span>
         </h1>
-        <p className="text-2xl font-light leading-10">
+        <p className="text-base md:text-lg font-light leading-7 md:leading-9">
           Bem-vindo ao Recoleta, o aplicativo que está revolucionando a forma
           como lidamos com resíduos. Nossa missão é conectar geradores de
           resíduos, recicladores e tratadores de resíduos em uma rede
@@ -36,7 +36,7 @@ export default function About() {
         >
           Apoie nossa causa
         </Button>
-      </div>
+      </section>
     </main>
   );
 }
