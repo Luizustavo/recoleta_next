1bc5bde01aeb1a6d0091b7ccaedf0e8bbde3ee3c
Luizustavo <luizg.ss@outlook.com>
2024-11-18 19:25:33 -0300

    feat: enhance dashboard with environmental impact metrics and update sidebar icons

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -12,16 +12,17 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import Button from '@/components/atoms/button';
+import Countup from 'react-countup';
 
 export default function Dashboard() {
   return (
     <div className="flex-1  ">
-      <main className="p-6">
+      <main className="p-3">
         <div className="grid sm:grid-cols-5 gap-5">
           <Card className="w-full col-span-2">
             <CardHeader>
               <CardTitle>Parabéns, Jane Doe! 🎉</CardTitle>
-              <CardDescription className="pt-3 flex flex-col text-sm">
+              <CardDescription className="pt-3 flex flex-col text-sm ">
                 <h1 className="text-success font-bold">
                   🎉 Você realizou 30 coletas este mês, superando a meta em
                   150%!{' '}
@@ -39,13 +40,56 @@ export default function Dashboard() {
                 </h1>
               </CardDescription>
             </CardHeader>
-            <CardContent>
+            <CardContent className="pt-10">
               <Button variant="primaryOutline">Compartilhar conquista</Button>
             </CardContent>
           </Card>
           <div className="col-span-2">
             <PieChartText />
           </div>
+          <Card className="w-full col-span-1">
+            <CardHeader>
+              <CardTitle>Impacto ambiental</CardTitle>
+              <CardDescription className="pt-3 flex flex-col text-sm gap-4">
+                <Card className="p-3 flex flex-col items-center justify-center">
+                  <h1 className=" font-bold text-primary-green">
+                    CO² evitados
+                  </h1>
+                  <Countup
+                    end={1658}
+                    suffix="kg"
+                    className="font-bold text-lg"
+                    delay={1}
+                    duration={5}
+                  />
+                </Card>
+                <Card className="p-3 flex flex-col items-center justify-center">
+                  <h1 className=" font-bold text-primary-blue">
+                    Água economizada
+                  </h1>
+                  <Countup
+                    end={3562}
+                    delay={1}
+                    duration={5}
+                    suffix="l"
+                    className="font-bold text-lg"
+                  />
+                </Card>
+                <Card className="p-3 flex flex-col items-center justify-center">
+                  <h1 className="font-bold text-primary-orange">
+                    Resíduos reciclados
+                  </h1>
+                  <Countup
+                    end={3542}
+                    delay={1}
+                    duration={5}
+                    suffix="kg"
+                    className="font-bold text-lg"
+                  />
+                </Card>
+              </CardDescription>
+            </CardHeader>
+          </Card>
         </div>
         <div className="grid sm:grid-cols-4 gap-5 mt-5 ">
           <div className="col-span-4 grid grid-cols-4 gap-5 ">
